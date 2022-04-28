92629e0d7ef8fb05eb6b4f6422b76741cc2cb486
Monisha G <83082244+Monishamoni03@users.noreply.github.com>
2022-04-28 18:17:03 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 //import HelloWorld from './components/HelloWorld.js';
 import Portals from './components/Portals.js';
+import HigherOrderComponent from './components/HigherOrderComponent';
+
 
 class App extends Component {
     render() {
         return(
             <div className='App'>
                  {/* <HelloWorld /> */}
-                 <Portals />
+                 {/* <Portals /> */}
+                 <HigherOrderComponent />
             </div>
         );
     }
