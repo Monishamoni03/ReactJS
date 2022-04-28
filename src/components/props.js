a28977da4b75b9ac2b32738680863cd7fa060e51
Monisha G <83082244+Monishamoni03@users.noreply.github.com>
2022-04-28 18:17:51 +0530

    Delete props.js

--- src/components/props.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-class props extends React.Component{
-  render(){
-    return <h1 style={{color:"green",textAlign:"center"}}>Welcome to the{this.props.name}</h1>
-  }
-}
-
-export default props;
