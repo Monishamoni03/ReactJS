a345727e69fbafb80002d9a315d9d0f1c81b62fd
Monisha G <83082244+Monishamoni03@users.noreply.github.com>
2022-04-28 18:19:37 +0530

    Delete classComponent.js

--- src/components/classComponent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-class classComponent extends React.Component{
-  render(){
-    return <h1>Welcome</h1>
-  }
-}
-
-export default classComponent;
