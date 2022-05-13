ae851b36697256d32ef35e7059c1619094dc7796
Monisha G <83082244+Monishamoni03@users.noreply.github.com>
2022-05-13 17:22:09 +0530

    Update Context.js

--- src/components/Context.js
+++ src/components/Context.js
@@ -1,3 +1,4 @@
+//context,consumer files
 import React from "react";
 
 const ContextName = React.createContext('message');
@@ -6,3 +7,24 @@ const ProviderValue = ContextName.Provider;
 const ConsumerValue = ContextName.Consumer;
 
 export {ProviderValue,ConsumerValue};
+
+//App.js
+
+import React, { Component } from 'react';
+import ContextName from './ContextName';
+import { ProviderValue } from './Context.js';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <ProviderValue value="Hello team">     {/* providing value*/}
+         <ContextName />  
+        </ProviderValue>                     
+      </div>
+    )
+  }
+}
+
+export default App;
